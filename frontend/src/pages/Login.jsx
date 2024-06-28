c099dd8ce723867129f1022348e77dce735c94fa
simanto848 <simanto848@gmail.com>
2024-06-28 17:09:12 +0600

    complete sign-up design and fix login design

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,64 +1,86 @@
 import { useState } from "react";
-import { FaEye } from "react-icons/fa";
-import { FaEyeSlash } from "react-icons/fa";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import loginIcon from "../assest/signin.gif";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   return (
     <section id="login">
       <div className="mx-auto container px-4 mt-3">
-        <div className="bg-white p-2 py-5 w-full max-w-sm mx-auto">
+        <div className="bg-white p-5 w-full max-w-sm mx-auto rounded-lg shadow-md">
           <div className="w-20 h-20 mx-auto">
             <img src={loginIcon} alt="login icon" />
           </div>
-          <form className="pt-6">
-            <div className="grid">
-              <label htmlFor="">Email:</label>
-              <div className="bg-slate-100 p-2">
+          <form className="pt-6" onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="email" className="block mb-2">
+                Email:
+              </label>
+              <div className="bg-slate-100 p-2 rounded-md">
                 <input
                   type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
                   placeholder="Enter email..."
                   className="w-full h-full outline-none bg-transparent"
+                  required
                 />
               </div>
             </div>
-            <div>
-              <label htmlFor="">Password:</label>
-              <div className="bg-slate-100 p-2 flex">
+            <div className="mb-4">
+              <label htmlFor="password" className="block mb-2">
+                Password:
+              </label>
+              <div className="bg-slate-100 p-2 rounded-md flex items-center">
                 <input
                   type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
                   placeholder="Enter password..."
                   className="w-full h-full outline-none bg-transparent"
+                  required
                 />
-                <div className="cursor-pointer">
-                  <span>
-                    {showPassword ? (
-                      <FaEyeSlash
-                        onClick={() => setShowPassword(!showPassword)}
-                      />
-                    ) : (
-                      <FaEye onClick={() => setShowPassword(!showPassword)} />
-                    )}
-                  </span>
+                <div
+                  className="cursor-pointer ml-2"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
                 </div>
               </div>
               <Link
                 to="/forgot-password"
                 className="block mt-2 w-fit ml-auto hover:underline hover:text-red-600 transition-all text-sm text-gray-500"
               >
-                Forgot Password ?
+                Forgot Password?
               </Link>
             </div>
 
-            <button className="bg-red-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:bg-red-700 hover:scale-110 transition-all mx-auto block mt-6">
+            <button
+              type="submit"
+              className="bg-red-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:bg-red-700 hover:scale-110 transition-all mx-auto block mt-6"
+            >
               Login
             </button>
           </form>
-          <p className="my-5">
-            Don&apos;t have account ?{" "}
+          <p className="my-5 text-center">
+            Don&apos;t have an account?{" "}
             <Link
               to="/sign-up"
               className="text-red-600 hover:text-red-700 hover:underline transition-all"
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,165 @@
-import React from "react";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import loginIcon from "../assest/signin.gif";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <section id="signup">
+      <div className="mx-auto container px-4 mt-3">
+        <div className="bg-white p-5 w-full max-w-sm mx-auto rounded-lg shadow-md">
+          <div className="w-20 h-20 mx-auto">
+            <img src={loginIcon} alt="signup icon" />
+          </div>
+          <form className="pt-6" onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="firstName" className="block mb-2">
+                First Name
+              </label>
+              <div className="bg-slate-100 p-2 rounded-md">
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  placeholder="Enter first name..."
+                  className="w-full h-full outline-none bg-transparent"
+                  required
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="lastName" className="block mb-2">
+                Last Name
+              </label>
+              <div className="bg-slate-100 p-2 rounded-md">
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  placeholder="Enter last name..."
+                  className="w-full h-full outline-none bg-transparent"
+                  required
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="phone" className="block mb-2">
+                Phone
+              </label>
+              <div className="bg-slate-100 p-2 rounded-md">
+                <input
+                  type="text"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  placeholder="Enter phone number..."
+                  className="w-full h-full outline-none bg-transparent"
+                  required
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block mb-2">
+                Email
+              </label>
+              <div className="bg-slate-100 p-2 rounded-md">
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Enter email..."
+                  className="w-full h-full outline-none bg-transparent"
+                  required
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block mb-2">
+                Password
+              </label>
+              <div className="bg-slate-100 p-2 rounded-md flex items-center">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Enter password..."
+                  className="w-full h-full outline-none bg-transparent"
+                  required
+                />
+                <div
+                  className="cursor-pointer ml-2"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </div>
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="confirmPassword" className="block mb-2">
+                Confirm Password
+              </label>
+              <div className="bg-slate-100 p-2 rounded-md flex items-center">
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  placeholder="Confirm password..."
+                  className="w-full h-full outline-none bg-transparent"
+                  required
+                />
+                <div
+                  className="cursor-pointer ml-2"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                </div>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="bg-red-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:bg-red-700 hover:scale-110 transition-all mx-auto block mt-6"
+            >
+              Sign Up
+            </button>
+          </form>
+          <p className="my-5 text-center">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="text-red-600 hover:text-red-700 hover:underline transition-all"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Signup;
