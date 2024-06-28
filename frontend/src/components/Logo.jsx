5617305c77345d25229f2d1b1eeda34306a2e760
simanto848 <simanto848@gmail.com>
2024-06-28 15:17:31 +0600

    Navbar

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = ({ w, h }) => {
+  return (
+    <div className="text-2xl font-bold">
+      <span className="text-red-600">E</span>
+      <span>commerce</span>
+    </div>
+  );
+};
+
+export default Logo;
